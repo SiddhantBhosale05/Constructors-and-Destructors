aaa915c19453b143284697b870a225c8affcfa47
Siddhant Bhosale <siddhant.bhosale05@gmail.com>
2024-10-12 16:23:30 +0530

    Parameterizes.cpp

--- Parameterizes.cpp
+++ Parameterizes.cpp
@@ -1,6 +1,6 @@
 //Name:Siddhant Kiran Bhosale
 //PRN:23070123038
-//Aim:Implemention of Constructors and Destructors to perform Parameterizes
+//Aim:Implementation of Constructors and Destructors to perform Parameterizes
 
 #include <iostream>
 using namespace std;
